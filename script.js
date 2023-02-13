2e6f9fd7e5fb6d0b8f7ceb15e64a058af7011c1f
Abhishek <28715657+proabhishek@users.noreply.github.com>
2023-02-13 22:43:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,19 +3,24 @@ var btn = document.getElementById("btn");
 var numPtag = document.getElementById("num");
 var res = document.getElementById("respond");
 var inputValue = document.getElementById("guess").value;
-var diff = Number(inputValue) - Number(numPtag.value);
 
-if(Math.abs(diff) <= 5) {
+
+function checkGame () {
+	var diff = Number(inputValue) - Number(numPtag.value);
+	if(Math.abs(diff) <= 5) {
 	res.textContent = "Hot";
+	}
+	else {
+		res.textContent = "cold";
+	}
 }
-else {
-	res.textContent = "cold";
-}
+
+checkGame();
 
 function generateRandomNumber() {
     var no = Math.floor(Math.random() * 54) - 14;
 	numPtag.textContent = no;
-	
+	checkGame();
 }
 
 btn.addEventListener("click", generateRandomNumber);
